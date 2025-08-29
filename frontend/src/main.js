8c7d1afce87ae55123fd40938704f1d28e58faa9
Jordan Bell <jayymbell@gmail.com>
2025-08-29 14:24:36 -0500

    configure pinia

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,10 +1,13 @@
 import { createApp } from 'vue'
+import { createPinia } from 'pinia'
 import './style.css'
 import App from './App.vue'
 import vuetify from '../plugins/vuetify'
 
 const app = createApp(App)
 
+const pinia = createPinia()
+app.use(pinia)
 
 app.use(vuetify)
 
