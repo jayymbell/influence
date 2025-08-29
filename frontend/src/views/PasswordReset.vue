98354b33e7df9de02f9a2ea977788168041ca73c
Jordan Bell <jayymbell@gmail.com>
2025-08-29 16:05:47 -0500

    password reset

---
+++ frontend/src/views/PasswordReset.vue
@@ -0,0 +1,51 @@
+<template>
+    <v-container>
+      <h1>Reset Password</h1>
+      <v-form @submit.prevent="requestPasswordReset">
+        <v-text-field v-model="email" label="Email" required></v-text-field>
+              <v-row>
+        <v-col cols="7">
+        </v-col>
+        <v-col  class="text-right">
+          <v-btn type="submit" block>Submit</v-btn>
+        </v-col>
+      </v-row>
+      </v-form>
+    </v-container>
+  </template>
+  
+  <script>
+  import api from '../services/api'
+  import { useRoute, useRouter } from 'vue-router'
+  import { inject } from 'vue'
+  
+  export default {
+    data() {
+      return {
+        email: ''
+      }
+    },
+    setup() {
+      const route = useRoute()
+      const router = useRouter()
+      const showSnackbar = inject('showSnackbar')
+      return { showSnackbar, router, route }
+    },
+    methods: {
+      async requestPasswordReset() {
+        try {
+          if(this.email === '') {
+          this.showSnackbar(['Please enter your email address.'], 'error')
+          return
+        }
+          const response = await api.post('/password', { user: { email: this.email } })
+          this.showSnackbar([response.data.status.message], 'success')
+          this.router.push({ name: 'Login' })
+        } catch (error) {
+          const errors = error.response.data.errors || ['An unknown error occurred']
+          this.showSnackbar(errors, 'error')
+        }
+      }
+    }
+  }
+  </script>
