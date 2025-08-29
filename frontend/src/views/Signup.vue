5f2cad675b301d8f6c1286fbec9806939cb2e15f
Jordan Bell <jayymbell@gmail.com>
2025-08-29 16:05:14 -0500

    sign up

---
+++ frontend/src/views/Signup.vue
@@ -0,0 +1,69 @@
+<template>
+  <v-container>
+    <h1>Sign Up</h1>
+    <v-form @submit.prevent="handleSignup">
+      <v-text-field
+        v-model="email"
+        label="Email"
+        type="email"
+        required
+      ></v-text-field>
+      <v-text-field
+        v-model="password"
+        label="Password"
+        type="password"
+        required
+      ></v-text-field>
+      <v-text-field
+        v-model="password_confirmation"
+        label="Confirm Password"
+        type="password"
+        required
+      ></v-text-field>
+            <v-row>
+        <v-col cols="7">
+
+        </v-col>
+        <v-col  class="text-right">
+          <v-btn type="submit" block>Submit</v-btn>
+        </v-col>
+      </v-row>
+
+    </v-form>
+  </v-container>
+</template>
+
+<script>
+import { useRouter } from 'vue-router'
+import { inject } from 'vue'
+import api from '../services/api'
+
+export default {
+  data() {
+    return {
+      email: '',
+      password: '',
+      password_confirmation: ''
+    }
+  },
+  setup() {
+    const router = useRouter()
+    const showSnackbar = inject('showSnackbar')
+    return { router, showSnackbar }
+  },
+  methods: {
+    async handleSignup() {
+      const data = {user: {email: this.email, password: this.password, password_confirmation: this.password_confirmation}}
+      try {
+        const response = await api.post('/signup',data)
+        this.showSnackbar([response.data.message], 'success')
+        this.router.push({ name: 'Login' })
+      } catch (error) {
+        console.log(error)
+        const errors = error.response.data.errors || ['An unknown error occurred']
+        this.showSnackbar(errors, 'error')
+      }
+    }
+  }
+}
+</script>
