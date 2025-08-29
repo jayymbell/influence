9df6bbca1cb5fa90e78f630e42761ce3d03d1a7c
Jordan Bell <jayymbell@gmail.com>
2025-08-29 14:10:41 -0500

    passwords controller

--- backend/app/controllers/users/passwords_controller.rb
+++ backend/app/controllers/users/passwords_controller.rb
@@ -1,34 +1,22 @@
-# frozen_string_literal: true
-
 class Users::PasswordsController < Devise::PasswordsController
-  # GET /resource/password/new
-  # def new
-  #   super
-  # end
+  respond_to :json
 
-  # POST /resource/password
-  # def create
-  #   super
-  # end
+  private
 
-  # GET /resource/password/edit?reset_password_token=abcdef
-  # def edit
-  #   super
-  # end
-
-  # PUT /resource/password
-  # def update
-  #   super
-  # end
-
-  # protected
-
-  # def after_resetting_password_path_for(resource)
-  #   super(resource)
-  # end
-
-  # The path used after sending reset password instructions
-  # def after_sending_reset_password_instructions_path_for(resource_name)
-  #   super(resource_name)
-  # end
+  def respond_with(resource, _opts = {})
+    if action_name == 'create'
+      render json: {
+        status: { code: 200, message: I18n.t('devise.passwords.send_paranoid_instructions') }
+      }
+    elsif resource.errors.blank?
+      render json: {
+        status: { code: 200, message: I18n.t('devise.passwords.updated_not_active') }
+      }
+    else
+      render json: {
+        status: { message: 'Password reset failed.' },
+        errors: resource.errors.full_messages
+      }, status: :unprocessable_entity
+    end
+  end
 end
