97db69aa0f3a2d0938f0920c2a1de0128a904c84
Jordan Bell <jayymbell@gmail.com>
2025-08-29 11:37:10 -0500

    add jwt secret key

--- backend/config/initializers/devise.rb
+++ backend/config/initializers/devise.rb
@@ -313,6 +313,13 @@ Devise.setup do |config|
 
     ## JWT configuration
   config.jwt do |jwt|
-    jwt.secret = Rails.application.credentials.devise_jwt_secret_key!
+    jwt.secret = Rails.application.credentials.jwt_secret_key!
+        jwt.dispatch_requests = [
+      ['POST', %r{^/login$}]
+    ]
+    jwt.revocation_requests = [
+      ['DELETE', %r{^/logout$}]
+    ]
+    jwt.expiration_time = 1.day.to_i
   end
 end
