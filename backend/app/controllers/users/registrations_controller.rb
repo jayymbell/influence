5da5ebbfc6b1a2315b98ac8b092bc49fe7bb3832
Jordan Bell <jayymbell@gmail.com>
2025-08-29 14:10:12 -0500

    registrations controller

--- backend/app/controllers/users/registrations_controller.rb
+++ backend/app/controllers/users/registrations_controller.rb
@@ -1,62 +1,44 @@
-# frozen_string_literal: true
-
 class Users::RegistrationsController < Devise::RegistrationsController
-  # before_action :configure_sign_up_params, only: [:create]
-  # before_action :configure_account_update_params, only: [:update]
+  respond_to :json
 
-  # GET /resource/sign_up
-  # def new
-  #   super
-  # end
+  def update
+    self.resource = resource_class.to_adapter.get!(send(:"current_#{resource_name}").to_key)
+    resource_updated = update_resource(resource, user_params)
+    yield resource if block_given?
+    if resource_updated
+      render json: {
+        status: 200,
+        message: I18n.t('devise.registrations.updated'),
+        user: UserSerializer.new(resource).serializable_hash[:data][:attributes]
+      }
+    else
+      render json: {
+        status: 422,
+        message: 'Account update failed.',
+        errors: resource.errors.full_messages
+      }, status: :unprocessable_entity
+    end
+  end
 
-  # POST /resource
-  # def create
-  #   super
-  # end
+  private
 
-  # GET /resource/edit
-  # def edit
-  #   super
-  # end
+  def respond_with(resource, _opts = {})
+    if resource.persisted?
 
-  # PUT /resource
-  # def update
-  #   super
-  # end
+      render json: {
+        status: 200,
+        message: I18n.t('devise.registrations.signed_up_but_unconfirmed')
+      }
+    else
+      render json: {
+        status: 422,
+        message: 'Sign up failed.',
+        errors: resource.errors.full_messages
+      }, status: :unprocessable_entity
+    end
+  end
 
-  # DELETE /resource
-  # def destroy
-  #   super
-  # end
-
-  # GET /resource/cancel
-  # Forces the session data which is usually expired after sign
-  # in to be expired now. This is useful if the user wants to
-  # cancel oauth signing in/up in the middle of the process,
-  # removing all OAuth session data.
-  # def cancel
-  #   super
-  # end
-
-  # protected
-
-  # If you have extra params to permit, append them to the sanitizer.
-  # def configure_sign_up_params
-  #   devise_parameter_sanitizer.permit(:sign_up, keys: [:attribute])
-  # end
-
-  # If you have extra params to permit, append them to the sanitizer.
-  # def configure_account_update_params
-  #   devise_parameter_sanitizer.permit(:account_update, keys: [:attribute])
-  # end
-
-  # The path used after sign up.
-  # def after_sign_up_path_for(resource)
-  #   super(resource)
-  # end
-
-  # The path used after sign up for inactive accounts.
-  # def after_inactive_sign_up_path_for(resource)
-  #   super(resource)
-  # end
+  def user_params
+    params.require(:user).permit(:email, :password, :password_confirmation, :current_password)
+  end
 end
