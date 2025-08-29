32577399c147d914061846ac432507edf9f847a4
Jordan Bell <jayymbell@gmail.com>
2025-08-29 16:06:32 -0500

    routes in app view

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,30 +1,75 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
+import useUserStore from './stores/UserStore'
+import { computed, ref, provide } from 'vue'
+import { useRoute, useRouter } from 'vue-router'
+
+const userStore = useUserStore()
+const isLoggedIn = computed(() => userStore.isLoggedIn)
+const route = useRoute()
+const router = useRouter()
+
+// Snackbar state
+const snackbar = ref(false)
+const snackbarColor = ref('error')
+const snackbarMessages = ref([])
+
+const goToLogin = () => {
+  router.push({ name: 'Login' })
+}
+
+const goToSignUp = () => {
+  router.push({ name: 'Signup' })
+}
+
+const goToAccount = () => {
+  router.push({ name: 'Account' })
+}
+
+const goToDashboard = () => {
+  router.push({ name: 'Dashboard' })
+}
+
+const showSnackbar = (messages, color = 'error') => {
+  snackbarMessages.value = messages
+  snackbarColor.value = color
+  snackbar.value = true
+}
+
+const logout = async () => {
+  const response = await userStore.logout()
+  showSnackbar([response.data.message], 'success')
+  router.push({ name: 'Login' })
+}
+
+
+// Provide the showSnackbar function to child components
+provide('showSnackbar', showSnackbar)
 </script>
 
 <template>
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+  <v-app>
+    <v-app-bar theme="dark">
+      <v-toolbar-title><a @click="goToDashboard">My App</a></v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-btn text v-if="isLoggedIn" @click="goToAccount">Account</v-btn>
+      <v-btn icon v-if="isLoggedIn" @click="logout" style="margin-left: 20px;"><v-icon color="white" icon="mdi-power" size="large"></v-icon></v-btn>
+      <v-btn text v-else-if="!isLoggedIn && route.name !== 'Login'" @click="goToLogin">Log In</v-btn>
+      <v-btn text v-else-if="route.name === 'Login'" @click="goToSignUp">Sign Up</v-btn>
+    </v-app-bar>
+    <v-main>
+      <router-view />
+    </v-main>
+    <v-snackbar v-model="snackbar" :timeout="2000" :color="snackbarColor" location="top right" style="margin-right: 5px;">
+      <ul v-if="snackbarMessages.length > 1" style="padding-left: 10px;">
+        <li v-for="message in snackbarMessages" :key="message">{{ message }}</li>
+      </ul>
+      <span v-if="snackbarMessages.length === 1">{{ snackbarMessages[0] }}</span>
+    </v-snackbar>
+  </v-app>
 </template>
 
 <style scoped>
 .logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
+  /* existing styles */
 }
 </style>
---
+++ frontend/src/router/index.js
@@ -0,0 +1,87 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import useUserStore from '../stores/UserStore'
+
+import Login from '../views/Login.vue'
+import Dashboard from '../views/Dashboard.vue'
+import SignUp from '../views/Signup.vue'
+import Confirmation from '../views/Confirmation.vue'
+import PasswordEdit from '../views/PasswordEdit.vue'
+import PasswordReset from '../views/PasswordReset.vue'
+import Account from '../views/Account.vue'
+
+const routes = [
+  {
+    path: '/login',
+    name: 'Login',
+    component: Login,
+    beforeEnter: (to, from, next) => {
+      const userStore = useUserStore()
+      if (userStore.isLoggedIn) {
+        next({ name: 'Dashboard' })
+      } else {
+        next()
+      }
+    }
+  },
+  {
+    path: '/signup',
+    name: 'Signup',
+    component: SignUp,
+    beforeEnter: (to, from, next) => {
+      const userStore = useUserStore()
+      if (userStore.isLoggedIn) {
+        next({ name: 'Dashboard' })
+      } else {
+        next()
+      }
+    }
+  },
+  {
+    path: '/',
+    name: 'Dashboard',
+    component: Dashboard,
+    beforeEnter: (to, from, next) => {
+      const userStore = useUserStore()
+      if (userStore.isLoggedIn) {
+        next()
+      } else {
+        next({ name: 'Login' })
+      }
+    }
+  },
+  {
+    path: '/confirmation',
+    name: 'Confirmation',
+    component: Confirmation
+  },
+  {
+    path: '/password-reset',
+    name: 'PasswordReset',
+    component: PasswordReset
+  },
+  { 
+    path: '/password/edit',
+    name: 'PasswordEdit',
+    component: PasswordEdit
+  },
+  {
+    path: '/account',
+    name: 'Account',
+    component: Account,
+    beforeEnter: (to, from, next) => {
+      const userStore = useUserStore()
+      if (userStore.isLoggedIn) {
+        next()
+      } else {
+        next({ name: 'Login' })
+      }
+    }
+  }
+]
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes
+})
+
+export default router
