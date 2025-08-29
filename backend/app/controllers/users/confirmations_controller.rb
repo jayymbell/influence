0269e778e9fb12c6bedb229e07108eb93b705344
Jordan Bell <jayymbell@gmail.com>
2025-08-29 14:10:21 -0500

    confirmations controller

--- backend/app/controllers/users/confirmations_controller.rb
+++ backend/app/controllers/users/confirmations_controller.rb
@@ -1,30 +1,19 @@
-# frozen_string_literal: true
-
 class Users::ConfirmationsController < Devise::ConfirmationsController
-  # GET /resource/confirmation/new
-  # def new
-  #   super
-  # end
+  respond_to :json
 
-  # POST /resource/confirmation
-  # def create
-  #   super
-  # end
+  private
 
-  # GET /resource/confirmation?confirmation_token=abcdef
-  # def show
-  #   super
-  # end
+  def respond_with(resource, _opts = {})
+    if resource.errors.blank?
 
-  # protected
-
-  # The path used after resending confirmation instructions.
-  # def after_resending_confirmation_instructions_path_for(resource_name)
-  #   super(resource_name)
-  # end
-
-  # The path used after confirmation.
-  # def after_confirmation_path_for(resource_name, resource)
-  #   super(resource_name, resource)
-  # end
+      render json: {
+        status: { code: 200, message: 'Email confirmed.' }
+      }
+    else
+      render json: {
+        status: { message: 'Confirmation failed.' },
+        errors: resource.errors.collect(&:type).map{ |error| error.to_s.humanize }
+      }, status: :unprocessable_entity
+    end
+  end
 end
