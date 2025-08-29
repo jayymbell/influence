108fb12788e89b614cb7b3fd68d293ccc494fb20
Jordan Bell <jayymbell@gmail.com>
2025-08-29 14:10:32 -0500

    sessions controller

--- backend/app/controllers/users/sessions_controller.rb
+++ backend/app/controllers/users/sessions_controller.rb
@@ -1,27 +1,45 @@
-# frozen_string_literal: true
-
 class Users::SessionsController < Devise::SessionsController
-  # before_action :configure_sign_in_params, only: [:create]
+  respond_to :json
 
-  # GET /resource/sign_in
-  # def new
-  #   super
-  # end
+  private
 
-  # POST /resource/sign_in
-  # def create
-  #   super
-  # end
+  def respond_with(resource, _opt= {})
+    if resource.persisted?
+      @token = request.env['warden-jwt_auth.token']
+      headers['Authorization'] = @token
 
-  # DELETE /resource/sign_out
-  # def destroy
-  #   super
-  # end
+      render json: {
+          status: 200, 
+          message: I18n.t('devise.sessions.signed_in'),
+          token: @token,
+          user: UserSerializer.new(resource).serializable_hash[:data][:attributes]
+      }, status: :ok
+    else
+      render json: {
+          status: 401,
+          message: I18n.t('devise.failure.invalid', authentication_keys: 'token')
+      }, status: :unauthorized
+    end
+  end
 
-  # protected
+  def respond_to_on_destroy
+    if request.headers['Authorization'].present?
+      jwt_payload = JWT.decode(request.headers['Authorization'].split.last, Rails.application.credentials.jwt_secret_key!).first
+      current_user = User.find(jwt_payload['sub'])
+    end
 
-  # If you have extra params to permit, append them to the sanitizer.
-  # def configure_sign_in_params
-  #   devise_parameter_sanitizer.permit(:sign_in, keys: [:attribute])
-  # end
+    puts "CURRENT USER: #{current_user.inspect}"
+
+    if current_user
+      render json: {
+        status: 200,
+        message: I18n.t('devise.sessions.signed_out')
+      }, status: :ok
+    else
+      render json: {
+        status: 401,
+        message: I18n.t('devise.errors.messages.not_found')
+      }, status: :unauthorized
+    end
+  end
 end
