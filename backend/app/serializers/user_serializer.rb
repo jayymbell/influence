64a396a27615c1ee49c0509c41128b028099e92b
Jordan Bell <jayymbell@gmail.com>
2025-08-30 21:14:03 -0500

    adjust user serializer

--- backend/app/serializers/user_serializer.rb
+++ backend/app/serializers/user_serializer.rb
@@ -1,8 +1,9 @@
 class UserSerializer
   include JSONAPI::Serializer
   attributes :id, :email, :created_at
+  
 
   attribute :roles do |user|
-    user.roles.pluck(:name)
+    user.roles.select(:id, :name, :description)
   end
 end
