46d10602fdb0d2dbe3f1f942f276cbbea6b5c422
Jordan Bell <jayymbell@gmail.com>
2025-08-30 21:13:45 -0500

    add user controller routes

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -14,7 +14,9 @@ Rails.application.routes.draw do
   namespace :users do
     resources :events, only: [:create]
   end
+
   resources :roles
+  resources :users, only: [:index]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
