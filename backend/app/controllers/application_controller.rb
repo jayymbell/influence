87ba259d26698f5f32ae067a86b1f63356b11133
Jordan Bell <jayymbell@gmail.com>
2025-09-01 11:10:20 -0500

    initialize pundit

--- backend/app/controllers/application_controller.rb
+++ backend/app/controllers/application_controller.rb
@@ -1,2 +1,11 @@
 class ApplicationController < ActionController::API
+  include Pundit::Authorization
+  rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
+
+  private
+
+  def user_not_authorized(exception)
+    puts exception.inspect
+    render json: {error: 'You do not have permission to access this'}, status: :forbidden
+  end
 end
