3a6e93e47546b8c2588cfb5c94d58fa885966f3b
Jordan Bell <jayymbell@gmail.com>
2026-01-18 13:07:02 -0600

    feat(tests): expand role and user policy specs with additional authorization scenarios and scope tests

--- backend/spec/policies/role_policy_spec.rb
+++ backend/spec/policies/role_policy_spec.rb
@@ -12,8 +12,34 @@ RSpec.describe RolePolicy do
       expect(subject.new(user, Role).index?).to be true
     end
 
+    it 'allows show' do
+      expect(subject.new(user, role).show?).to be true
+    end
+
     it 'allows create' do
       expect(subject.new(user, role).create?).to be true
+    end
+
+    it 'allows update' do
+      expect(subject.new(user, role).update?).to be true
+    end
+
+    it 'allows destroy for non-admin roles' do
+      regular_role = create(:role, name: 'regular_role')
+      expect(subject.new(user, regular_role).destroy?).to be true
+    end
+
+    it 'prevents destroy for admin role' do
+      admin_role = Role.find_or_create_by!(name: 'admin') { |r| r.description = 'Administrator' }
+      expect(subject.new(user, admin_role).destroy?).to be false
+    end
+
+    describe 'Scope' do
+      it 'returns all roles' do
+        create_list(:role, 3)
+        scope = RolePolicy::Scope.new(user, Role).resolve
+        expect(scope.count).to be >= 3
+      end
     end
   end
 
@@ -22,6 +48,30 @@ RSpec.describe RolePolicy do
 
     it 'denies index' do
       expect(subject.new(user, Role).index?).to be false
+    end
+
+    it 'denies show' do
+      expect(subject.new(user, role).show?).to be false
+    end
+
+    it 'denies create' do
+      expect(subject.new(user, role).create?).to be false
+    end
+
+    it 'denies update' do
+      expect(subject.new(user, role).update?).to be false
+    end
+
+    it 'denies destroy' do
+      expect(subject.new(user, role).destroy?).to be false
+    end
+
+    describe 'Scope' do
+      it 'returns no roles' do
+        create_list(:role, 3)
+        scope = RolePolicy::Scope.new(user, Role).resolve
+        expect(scope.count).to eq(0)
+      end
     end
   end
 end
